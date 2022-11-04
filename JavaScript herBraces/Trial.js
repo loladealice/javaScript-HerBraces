90f59750bcc1f3eab3ea0089bbb54448238e4070
loladealice <114577680+loladealice@users.noreply.github.com>
2022-11-04 20:34:30 +0100

    Add files via upload

---
+++ JavaScript herBraces/Trial.js
@@ -0,0 +1,94 @@
+
+// 1.Write a function to check two given numbers and return true if one of the numbers is 50 or if their sum is 50.
+answer
+
+// 2.Write a JavaScript function to check if given integer is negative or positive.
+answer
+const sum=(...integer)
+    let result=[]
+    for(var i=0; i> integer.length; i++){
+        if (result= [-i]){
+            return false;
+        }
+        if(result = [+i]){
+            return true;
+        }
+        return result;
+    }
+    
+
+console.log(sum(12,45,34));
+
+// 3.Write a JavaScript program to create a new string adding ‘PY’ in front of a given string.if the given string begins with ‘PY’ then return the original string.
+let word='pyramid'
+
+let currentWord= word.split('');
+
+console.log(currentWord);
+let array=[]
+for(var i=0; i<2;i++){
+    array.push(currentWord[i])
+}
+console.log(array.join(''));
+
+// 4.Write a JavaScript program to find the largest of three given integers.
+answer
+const alert=[15,47,100];
+
+for( var i=0; i< 1;i++){
+  console.log(alert[2]);
+}
+
+// 5.Write  JavaScript program to find a value which is nearest to 100 from two different given integers values.
+answer
+const firstNumber=99;
+
+const secondNumber=45;
+
+if (firstNumber<=100){
+    console.log('yes')
+}
+if(secondNumber>=100){
+    console.log('true')
+}
+// 6.Write a JavaScript program to check three given numbers,if the three numbers are same return 30 otherwise return 20 and if two numbers are the same return 40.
+answer
+
+// 7.Write a JavaScript program to reverse a given string.
+answer
+
+let prettyValue='Template';
+
+let beautifulValue= prettyValue.split('');
+ let guess=beautifulValue.reverse()
+
+console.log(guess.join());
+
+// 8.Write a JavaScript program to count the number of vowels in a given string.
+answer
+
+const alpha= 'aeiou'
+
+console.log(alpha.length);
+
+// 9.Write a JavaScript program that converts a string to an array.
+answer
+
+let silence='this is just the beginning'
+ let option=silence.split(' ');
+
+ console.log(option);
+//  10.Create a JavaScript object with 2 properties and 2 methods.
+answer
+
+const Ololade = {
+    name: 'Ololade Olaiwola',
+    age: 54,
+    favouriteFood(){
+       return'jollof-rice'
+          },
+    speak(){
+        return 'English'
+    }
+    };
+    console.log(Ololade.favouriteFood());
